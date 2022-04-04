7fc37d512e3c688e381151b54bb65bcedc9001d9
ManuelDavidRamirez <Manuyagami95@gmail.com>
2022-04-04 15:37:32 -0300

    estructura terminada

--- pixelshop/app.js
+++ pixelshop/app.js
@@ -28,7 +28,6 @@ app.get('/login',(req,res)=> res.sendFile(path.resolve(__dirname,"views","login.
 app.get('/regsiter',(req,res)=> res.sendFile(path.resolve(__dirname,"views","register.html")));
 app.get('/productCart',(req,res)=> res.sendFile(path.resolve(__dirname,"views","productCart.html")));
 app.get('/productDetail',(req,res)=> res.sendFile(path.resolve(__dirname,"views","productDetail.html")));
-app.get('/*',(req,res)=> res.sendFile(path.resolve(__dirname,"views","notFound.html")));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
