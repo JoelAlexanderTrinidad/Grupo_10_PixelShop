b73cbd132b1d3549abe4f2d9f7f977fdb45a24c0
Agusflor <agusflormamani@gmail.com>
2022-05-05 13:04:41 -0300

    develop

--- pixelshop/controllers/productController.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    productDetail:(req,res)=>res.render('productDetail'),
-    productCart:(req,res)=>res.render('productCart'),
-    formCrear:(req,res)=>res.render('formCrear'),
-    formEdit:(req,res)=>res.render('formEdit')
-
-}
-
--- pixelshop/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* / */
-const{index}= require('../controllers/indexController')
-router
-      .get('/',index)
-
-module.exports = router;
