d7f81929f5c49c774beb9f54221c82e8967c3282
ManuelDavidRamirez <Manuyagami95@gmail.com>
2022-05-13 16:28:59 -0300

    comenzando sprint 4

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,4 +1,12 @@
+const products = require("../data/products.json");
+
 module.exports={
-    index:(req,res)=>res.render('index')
+    index:(req,res)=> {
+        const productfilter = products.filter(product => product.id === 1)
+        return res.send(productfilter)
+        return res.render('index', {
+            products
+        })
+    }
 }
 
