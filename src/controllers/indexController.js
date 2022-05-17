29a516467893517803883d10a4544a6d4ede3216
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2022-05-17 19:15:09 -0300

    arreglando vista

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,10 +1,8 @@
-const fs = require('fs');
-const path = require('path');
+const products = require("../data/products.json");
 
 module.exports={
     index:(req,res)=> {
         
-        const products = require("../data/products.json");
         const OfertasEspeciales = products.filter(product => product.category.toLocaleLowerCase() === "ofertas especiales");
         const Destacados = products.filter(product => product.category === "Destacados");
         const Recomendados = products.filter(product => product.category === "Recomendados");
