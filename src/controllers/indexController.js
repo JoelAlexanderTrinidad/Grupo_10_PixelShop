1bdc5ad09738be8bd8d6dfd80e04b388f14e3f4d
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2022-05-17 16:55:34 -0300

    agregando imagenes

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,11 +1,13 @@
-const products = require("../data/products.json");
+const fs = require('fs');
+const path = require('path');
 
 module.exports={
     index:(req,res)=> {
         
-        const OfertasEspeciales = products.filter(product => product.category === "Ofertas especiales")
-        const Destacados = products.filter(product => product.category === "Destacados")
-        const Recomendados = products.filter(product => product.category === "Recomendados")
+        const products = require("../data/products.json");
+        const OfertasEspeciales = products.filter(product => product.category.toLocaleLowerCase() === "ofertas especiales");
+        const Destacados = products.filter(product => product.category === "Destacados");
+        const Recomendados = products.filter(product => product.category === "Recomendados");
 
         return res.render('index', {
             products, OfertasEspeciales, Destacados, Recomendados
