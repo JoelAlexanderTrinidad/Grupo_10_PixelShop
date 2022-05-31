2f8804324ac94bd42aa16922b00f6bb4452d6d18
ManuelDavidRamirez <Manuyagami95@gmail.com>
2022-05-31 19:19:26 -0300

    profile: ruta, metodo vista y hardcodeo

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,15 @@
 module.exports={
     login:(req,res)=>res.render('login'),
-    register:(req,res)=>res.render('register')
+    register:(req,res)=>res.render('register'),
+    
+    profile: (req,res) => {
+        return res.render ("profile")
+
+        const users = JSON.parse(fs.readFileSync("./data/users.json","utf-8"));
+        const user = users.find(user => user.id === req.session.userLogin.id);
+        return res.render("profile",{
+            user
+        })
+    }
 }
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* /users */
-const {register,login}=require('../controllers/userController')
+const {register,login, profile}=require('../controllers/userController')
 router
       .get('/login',login)
       .get('/register',register)
+      .get('/profile', profile)
 
 module.exports = router;
