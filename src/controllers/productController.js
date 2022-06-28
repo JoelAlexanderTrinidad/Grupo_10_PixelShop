89bc004bb60add9f72f05926c64d541a2f86281e
Agusflor <agusflormamani@gmail.com>
2022-06-28 13:21:06 -0300

    search orm

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -59,61 +59,59 @@ module.exports={
         .then(res.redirect('/'))
         .catch(error=> console.log(error))          
     },
-   
+
+    //FALTA EDIT Y UPDATE ----> asociacion de genders
+    edit : (req,res) => {
+        const product = db.Product.findByPk(req.params.id);
+        const genders = db.Gender.findAll();
+        Promise.all([product, genders])
+        .then(([product,genders])=> {
+            return res.render('formEdit',{
+                product,
+                genders//FALTA ASOCIAR LOS GENEROS!!!
+            })
+        })
+        .catch(error=>console.log(error))
+    },   
     update: (req,res)=>{
-        const {id}=req.params;
-        const {name, price, category, discount, description, requeriment}= req.body;
+        const {name, price, category, discount, description, }= req.body;
 
-        let gender;
-
-        const product = products.find(product => product.id === +req.params.id)
-       
-        let productsModify =  products.map(product =>{
-             if(product.id === +req.params.id){
-                 let productModify={
-                ...product,
+        db.Product.update({
                 name: name,
                 price: +price,
                 discount:+discount,
                 category,
                 description: description,
                 img: req.file ? req.file.filename : product.img,
-                gender: !req.body.gender ? gender = product.gender : gender = typeof(req.body.gender) === 'string' ? [req.body.gender] : req.body.gender,
-                requeriment
-                 }
-                 return productModify
-                 
-                }
-                return product 
+               /*  gender: !req.body.gender ? gender = product.gender : gender = typeof(req.body.gender) === 'string' ? [req.body.gender] : req.body.gender, */
                 
-            })
+        },{
+            where: {
+                id: req.params.id
+            }
+        })
+        .then( res.redirect('/product/detail/' + product.id))
+        .catch(error=> console.log(error))
+        
+       
 
-            if(req.file){
-               if(fs.existsSync(path.resolve(__dirname,'..','public','images',product.img)) && product.img !== "noimage.jpeg"){
-                   fs.unlinkSync(path.resolve(__dirname,'..','public','images',product.img))
-               }
-           }        
-            
-            fs.writeFileSync(path.resolve(__dirname, '..', 'data', 'products.json'),JSON.stringify(productsModify, null, 3), 'utf8')
-
-        return res.redirect('/product/detail/' + product.id);
     },    
 
     search : (req,res) => {
-        const {keyword} = req.query;
-        const searchProduct = products.filter(product => product.name.toLowerCase().includes(keyword.toLowerCase()));
-
-        return res.render("result", {
-            products : searchProduct, keyword
+        let keyword = req.query
+        db.Product.findAll({
+            where:{
+                [Op.substring]:[keyword]
+            }
+        })
+        .then(products=>{
+            return res.render('result',{
+                products,
+                keyword
+            })
         })
     },
-    edit : (req,res) => {
-        const producto = JSON.parse(fs.readFileSync(path.resolve(__dirname, "..", "data", "products.json"), "utf-8"));
-
-        const product = producto.find(product => product.id === +req.params.id);
-       
-            res.render('formEdit', {product, genders});   
-    },
+    
     remove : (req,res) => {
         const productFilter = products.filter(product => product.id !== +req.params.id);
         const product = products.find(product => product.id === +req.params.id)
