588299479c7325394d4069c84abd492961504c20
Agusflor <agusflormamani@gmail.com>
2022-08-12 11:17:21 -0300

    admin- remove

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,6 +1,7 @@
-
 const db = require('../database/models');
 const { Op } = require("sequelize");
+const fs = require('fs')
+const path = require('path');
 
 module.exports={
 admin:(req,res)=> {
@@ -19,17 +20,50 @@ superAdmin:async(req,res)=> {
             rolId:{
             [Op.ne]: 3
             } 
-        }
+        },
     })
-    const rol = await db.Rol.findAll()
+    const rols = await db.Rol.findAll()
 
 return res.render('listUsers', {
                 users,
-                rol
+                rols
             })
        
    } catch (error) {
     console.log(error)
    }
+},
+update : async (req,res) => {
+    try {
+        const usuario = await User.findAll({
+            where : {
+                id: req.params.id
+            }
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}, 
+remove : async (req, res) =>{
+    try {
+        const user = await db.User.findAll({
+            where :{
+                id: req.params.id
+            },
+            attributes : ['imagenPerfil']
+        })
+        if(user[0].imagenPerfil !== 'no-image.png') {
+            fs.unlinkSync(path.resolve(__dirname, "..", "..", "public", "images", user[0].imagenPerfil))
+        }
+        await db.User.destroy({
+            where :{
+                id : req.params.id
+            }
+        })
+        return res.redirect('/admin/listUsers')
+    } catch (error) {
+        console.log(error)
+    }
 }
 }
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,11 +4,13 @@ var router = express.Router();
 /* / */
 const adminCheck = require('../middlewares/adminCheck');
 const superAdminCheck = require('../middlewares/superAdminCheck');
-const {admin, superAdmin} = require('../controllers/adminController')
+const {admin, superAdmin, remove} = require('../controllers/adminController')
 
 router
       .get('/listProduct', adminCheck, admin)
       .get('/listUsers', superAdminCheck, superAdmin)
+      .put('/update/:id', superAdminCheck, )
+      .delete('/remove/:id', superAdminCheck, remove)
       
 
 module.exports = router;
