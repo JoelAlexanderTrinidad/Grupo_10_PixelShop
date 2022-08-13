d98509c749a74f7cda9ec71da22b2dceddf58dcb
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2022-08-12 23:56:59 -0300

    se arregla id undefined

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -163,16 +163,19 @@ module.exports={
         }
 
     },
-    profile : (req, res) => {
-        User.findByPk(req.session.userLogin.id)
-        .then(user => {
+    profile : async (req, res) => {
+
+        try {
+           
+            const user = await User.findByPk(req.session.userLogin.id)
             const  fecha = !user.fecha ? null : moment(user.fecha).toISOString().slice(0,10)          
-            res.render("profile", {
+            return res.render("profile", {
                 user,
                 fecha
             })
-        })
-        .catch(error => console.log(error))
+        } catch (error) {
+            console.log(error)
+        }
     },
     removeUser : async (req,res) => {
         try {
