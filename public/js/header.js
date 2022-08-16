9cabf7c7402b7af998e5cd71e50e0ae9f7cd737c
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2022-08-16 17:46:49 -0300

    se arregla search escritorio

--- public/js/header.js
+++ public/js/header.js
@@ -1,5 +1,6 @@
 console.log('header success!!');
 
 document.getElementById('burger').addEventListener('click', function(e){
+    $('nav').classList.add('menu-burger--block')
     $('home__main').classList.toggle('menu-burger')
 })
