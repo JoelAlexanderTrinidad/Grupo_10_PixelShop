fc83281abe474fcdd2569509e94b8e9535a6074b
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2022-08-16 18:18:55 -0300

    regreso al search anterior

--- public/js/header.js
+++ public/js/header.js
@@ -1,6 +1,5 @@
 console.log('header success!!');
 
 document.getElementById('burger').addEventListener('click', function(e){
-    $('nav').classList.add('menu-burger--block')
-    $('home__main').classList.toggle('menu-burger')
+    $('nav').classList.toggle('header__nav')
 })
