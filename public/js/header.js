7d1b1d3eafa64878c89c8b2a263b15ba51a81060
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2022-08-17 17:17:08 -0300

    animacion burger hecho

--- public/js/header.js
+++ public/js/header.js
@@ -2,4 +2,5 @@ console.log('header success!!');
 
 document.getElementById('burger').addEventListener('click', function(e){
     $('nav').classList.toggle('header__nav')
+    $('nav').classList.toggle('burger')
 })
