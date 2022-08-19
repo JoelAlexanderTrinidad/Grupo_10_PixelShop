f51cbee0f43eda8268deb4874657bea9716b74c2
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2022-08-19 17:51:10 -0300

    delete user modal

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,30 +3,24 @@ console.log("profile.js success");
         $("deleteUser").addEventListener("submit", (e) => {
             e.preventDefault()
                 Swal.fire({
-                    title: 'Are you sure?',
-                    text: "You won't be able to revert this!",
+                    title: '¿Estás seguro/a?',
+                    text: "¡No podrás revertir esto!",
                     icon: 'warning',
                     showCancelButton: true,
                     confirmButtonColor: '#3085d6',
                     cancelButtonColor: '#d33',
-                    confirmButtonText: 'Yes, delete it!'
-                  }).then((result) => {
+                    confirmButtonText: 'Eliminar usuario',
+                    cancelButtonText: 'Cancelar'
+                }).then((result) => {
                             if (result.isConfirmed) {
                                 if(e){
                                     Swal.fire(
-                                        'Deleted!',
-                                        'Your file has been deleted.',
+                                        '¡Eliminado!',
+                                        'El usuario ha sido eliminado.',
                                         'success'
                                         )
                                         e.target.submit()
                                 }
-                                }
-                            
-                  })
-       /*  if(e){
-            $('deleteUser').addEventListener('submit', (e) => {
-                e.preventDefault()
-                e.target.submit()
-            })
-        } */
+                        }
+                })
     })
