284d21cb03763dffc76148ca1ca90ea83ff433e9
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2022-08-19 09:19:43 -0300

    modal eliminar

--- public/js/listProduct.js
+++ public/js/listProduct.js
@@ -1,9 +1,35 @@
-console.log("listProduct.js success");
+console.log("listProduct.js success!!");
 
-$("btn-delete").addEventListener("click", (e) => {
-    if (!confirm("¿Esta seguro que desea eliminar el producto?")) {
-        e.preventDefault()
+window.onload = function(){
+$("borrarProducto").addEventListener("submit", (e) => {
+    e.preventDefault()
+    if(e){
+        alert('FUNCIONA?')
+        /* Swal.fire({
+            title: '¿Estás seguro/a?',
+            text: "No podrás revertir esto!",
+            icon: 'warning',
+            cancelButtonText: 'Cancelar',
+            showCancelButton: true,
+            confirmButtonColor: '#d51818',
+            cancelButtonColor: '#4ed34c',
+            confirmButtonText: 'Borrar usuario'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Borrado!',
+                    'El usuario ha sido borrado',
+                    'success'
+                    )
+                    console.log(Swal.fire);
+            }
+            
+        }) */
+          /* setTimeout(function(){
+              e.target.submit()
+          }, 4000); */
     }
 })
+}
 
 
