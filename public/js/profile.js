ab7ca689325b710dc8e3abaf075969b8628a2b9a
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2022-08-18 22:44:23 -0300

    borrar usuario ventana modal

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,7 +1,35 @@
-console.log("profile.js success");
+console.log('profile.js susscess!!!');
 
-$("btn-delete").addEventListener("click", (e) => {
-    if (!confirm("¿Esta seguro que desea borrar su registro?")) {
+window.onload = function(){
+
+    $("borrar").addEventListener("submit", (e) => {
         e.preventDefault()
-    }
-})
+        if(e){
+            Swal.fire({
+                title: '¿Estás seguro/a?',
+                text: "No podrás revertir esto!",
+                icon: 'warning',
+                cancelButtonText: 'Cancelar',
+                showCancelButton: true,
+                confirmButtonColor: '#d51818',
+                cancelButtonColor: '#4ed34c',
+                confirmButtonText: 'Borrar usuario'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire(
+                        'Borrado!',
+                        'El usuario ha sido borrado',
+                        'success'
+                        )
+                        console.log(Swal.fire);
+                }
+                
+            })
+              setTimeout(function(){
+                  e.target.submit()
+              }, 4000);
+        }
+        
+    })
+    
+}
