55a8aa3ba00319a74c13e96e7f462c3ba76ea373
Agusflor <agusflormamani@gmail.com>
2022-08-23 16:21:42 -0300

    cambios api

--- public/js/explore.js
+++ public/js/explore.js
@@ -1,28 +1,34 @@
 console.log('explore success! c:');
-
-const searchGames = async (L)=>{
-    try {
-        let response = await fetch('/product/api/exploreProducts',{
-        method :'POST',
-        body : JSON.stringify({
-            letra : L
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
+let products = async () => {try {
+    let response = await fetch(`/product/api/exploreProducts?l=${target.value}`)
     let result = await response.json();
-        return result.data;
-
-    } catch (error) {
-        console.error
-    }
+    return result;
+    
+} catch (error) {
+    console.error
 }
+}
+    
+let leters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
-
-let leters = $('abc')
-
-leters.addEventListener('submit' ,function(e){
-    e.preventDefault()
-    console.log();
-});
+    leters.forEach(letra=>{
+        $('abc').innerHTML +=`<button class="ABC" id="busqueda_letra" name="l" value="${letra}">${letra}</button>`
+    })
+    $('busqueda_letra')&& $('busqueda_letra').addEventListener('click' ,async({target})=>{
+       
+        result.forEach(({id,img,discount,price})=>
+        $('showGames').innerHTML += `
+        <article>
+        <a href="/product/detail/${id}"><img src="/images/${img}" alt=""></a>
+        <div class="details">
+            <p>$${price}</p>
+            <span>${discount} OFF</span>
+            <div>
+                <i class="fas fa-cart-plus"></i>
+                <i class="fa-regular fa-heart"></i>
+            </div>
+        </div>
+    </article>`
+        )
+        
+        })
